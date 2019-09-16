02e3007e9f98f8467828c89d76b2f3cb1b2986ff
mark <bs255200@163.com>
2019-09-16 18:47:19 +0800

    9-16

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -7,7 +7,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    playerName:null
+    playInfo:null,
     },
 
   /**
@@ -33,25 +33,41 @@ Page({
           title: '加载中...',
         });
     gamesSignUp.get().then(res => { /**then是在执行完前面get()之后执行then之内的语句 */
+      res.data[0].starttime = res.data[0].starttime.toLocaleString();
+      res.data[0].endtime = res.data[0].endtime.toLocaleString(); /**将数据库中的date格式输出为字符串 */
+      res.data[0].latitude = res.data[0].fieldgeoinfo.latitude; 
+      res.data[0].longitude = res.data[0].fieldgeoinfo.longitude;  /**构建map需要的经纬度 */
+      res.data[0].fieldgeoinfo = [{
+            id : 0,
+            iconPath: "../../images/icons/gps.png",
+            latitude:  res.data[0].fieldgeoinfo.latitude,
+            longitude:  res.data[0].fieldgeoinfo.longitude,
+            width:30,
+            height:30
+      }]/**构建地点marker坐标 */
       this.setData({
-      playerName : res.data
-    }),/**这里的逗号起到连接作用，和之前的语句形成一个语句串 */
-    res => {callback();}/**构建一个箭头函数把callbcak作为返回值，实现了可以空值调用getData函数也有返回值的目的 */
-    wx.hideLoading();
+        playInfo:res.data
+      }),/**这里的逗号起到连接作用，和之前的语句形成一个语句串 */
+     res => {callback();}/**构建一个箭头函数把callbcak作为返回值，实现了可以空值调用getData函数也有返回值的目的 */
+      wx.hideLoading();
     })
   },
 
-  onSubmit: function(e){
-    gamesSignUp.add({
-      data:{
-        player:e.detail.value.player
-      }
-    }).then(res=>{
-      // wx.showToast({
-      //   title: '报名成功',
-      //   icon: 'success',
-      // });
+  // onSubmit: function(e){
+  //   gamesSignUp.add({
+  //     data:{
+  //       player:e.detail.value.player
+  //     }
+  //   }).then(res=>{
+  //     // wx.showToast({
+  //     //   title: '报名成功',
+  //     //   icon: 'success',
+  //     // });
         
-    })
+  //   })
+  // },
+  onRefresh:function(){
+    this.getData();
   }
+
 })
