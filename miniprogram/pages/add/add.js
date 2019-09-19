6272784a409576ff4d96fadc1dc56710cf10cc40
Markiiiiiiii <bs255200@163.com>
2019-09-19 22:42:46 +0800

    09-19-5

--- miniprogram/pages/add/add.js
+++ miniprogram/pages/add/add.js
@@ -12,9 +12,12 @@ Page({
       displayValue3:'请选择报名截止时间',
       lang:'zh_CN',
       endTimeStart:null,
-      nowTime:null
+      nowTime:null,
+      footballFileAddress:null
   },
-
+  pageData:{
+      locationObj:{}
+  },
   onLoad: function (options) {
     var time = util.formatTime(new Date());  
     this.setData({
@@ -48,12 +51,12 @@ onConfirmStart(e) {
     this.setData({
       endTimeStart: e.detail.label
     })
-    console.log(`onConfirm${index}`, e.detail)
+    // console.log(`onConfirm${index}`, e.detail)
 },
 onConfirm(e) {
-  const { index, mode } = e.currentTarget.dataset
+  const { index, mode } = e.currentTarget.dataset 
   this.setValue(e.detail, index, mode)
-  console.log(`onConfirm${index}`, e.detail.label)
+  // console.log(`onConfirm${index}`, e.detail.label)
 },
 /**时间戳的连选 */
 onVisibleChange(e) {
@@ -63,4 +66,25 @@ onClick() {
     this.setData({ visible: true })
 },
 
+/**选择地理位置 */
+chooseLocation: function(e){
+    wx.chooseLocation({
+      success: (res) => {
+        console.log(res)
+        let locationObj = {
+          latitude: res.latitude,
+          longitude: res.longitude,
+          name: res.name,
+          address: res.address
+        }
+        this.pageData.locationObj = locationObj ;/**获取到地理位置信息，构建页内对象 */
+        this.setData({
+          footballFileAddress:res.name
+        });/**返回页面地理名称 */
+      },
+      fail: () => {},
+      complete: () => {}
+    });
+      
+}
 })
