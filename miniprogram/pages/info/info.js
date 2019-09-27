b845f1ccb7c959127ff7560367b143cf5facc077
mark <bs255200@163.com>
2019-09-27 17:38:07 +0800

    09-27 v0.0.1  数据清洗 错误点

--- miniprogram/pages/info/info.js
+++ miniprogram/pages/info/info.js
@@ -1,7 +1,10 @@
 // miniprogram/pages/index/index.js
-const db = wx.cloud.database();
-const gamesSignUp = db.collection('gamesSignUp');
-const gamesPlayer = db.collection('gamesPlayer');
+const db = wx.cloud.database({
+    env:'biggoose-d92594'
+});
+const _ = db.command;
+// const gamesSignUp = db.collection('gamesSignUp');
+// const gamesPlayer = db.collection('gamesPlayer');
 
 Array.prototype.indexOf = function (val){
   for (var i = 0;i<this.length;i++){
@@ -32,12 +35,12 @@ Page({
   pageData:{
       indexId:null,
       playerArray:[],
-      _userInfo:{},
-      _alluserlists:[],
-      _alluserlist:[]
+      _userInfo:{}
   },
-
-  onLoad: function (options) {
+  userData:{
+    _openid:[]
+  },
+onLoad: function (options) {
     wx.getSetting({
     success: (result)=>{
       if(result.authSetting['scope.userInfo']){
@@ -78,73 +81,145 @@ Page({
   },
 
 /**数据获取函数 */
-  getData: function(value,uid){
-    if(!value){
-      value = res=>{} /**如果callback不是一个函数则使用箭头函数构造一个空函数 */
-    }
-    wx.showLoading({
-          title: '加载中...',
-        });
-    var _playerlists=[];/**数据库内报名后的用户openid */
-    gamesSignUp
-    .where({_id:value})
-    .get().then(res => { 
-      /**then是在执行完前面get()之后执行then之内的语句 */
+getData: function(value,uid){
       
-      res.data[0].cutofftime = res.data[0].cutofftime.toLocaleString();
-      res.data[0].starttime = res.data[0].starttime.toLocaleString();
-      res.data[0].endtime = res.data[0].endtime.toLocaleString(); /**将数据库中的date格式输出为字符串 */
-      res.data[0].latitude = res.data[0].fieldgeoinfo.latitude; 
-      res.data[0].longitude = res.data[0].fieldgeoinfo.longitude;  /**构建map需要的经纬度 */
-      // res.data[0].fieldgeoinfo = [{
-      //       id : 0,
-      //       iconPath: "../../images/icons/gps.png",
-      //       latitude:  res.data[0].fieldgeoinfo.latitude,
-      //       longitude:  res.data[0].fieldgeoinfo.longitude,
-      //       width:30,
-      //       height:30
-      // }]
-      /**构建地点marker坐标 */
-      res.data[0].playerAvatars = {};/**构建一个空对象，获取用户头像 */
-      _playerlists = res.data[0].playerlist;
-      console.log(_playerlists)
-      const _ = db.command
-      /**查询player表返回用户信息 */
-      gamesPlayer.where({
-        _openid:_.in(_playerlists)
-      })
-      .get()
-      .then(
-        rest=>(  
-          console.log(rest),
-          this.setData({
-            playernames:rest.data
-          })
-          ))
-      this.setData({
-        playInfo:res.data
-      }),
-     res => {value();}
-     /**这里的逗号起到连接作用，和之前的语句形成一个语句串 */
-     /**构建一个箭头函数把callbcak作为返回值，实现了可以空值调用getData函数也有返回值的目的 */
-      wx.hideLoading();
-    /**判断当前用户的openid是否在已报名列中，如果没有则显示“报名这个活动”，如果存在则显示“退出这个活动” */
-      this.pageData._alluserlist =_playerlists
-      if(_playerlists.includes(uid)){
-        this.setData({
-          nickName:'insider',
-          uopenid:uid,
-          gameid:value
-        })
-      }else{
-        this.setData({
-          nickName:'nin'
-        })
+      if(!value){
+        value = res=>{} /**如果callback不是一个函数则使用箭头函数构造一个空函数 */
       }
+      wx.showLoading({
+            title: '加载中...',
+          });
+      var _playerlists=[];/**用于存储查询返回的数据库内已报名的用户openid */
+      db.collection('gamesSignUp').where({_id:value})
+      .get().then(res => { 
+          /**then是在执行完前面get()之后执行then之内的语句 */
+          
+          res.data[0].cutofftime = res.data[0].cutofftime.toLocaleString();
+          res.data[0].starttime = res.data[0].starttime.toLocaleString();
+          res.data[0].endtime = res.data[0].endtime.toLocaleString(); /**将数据库中的date格式输出为字符串 */
+          res.data[0].latitude = res.data[0].fieldgeoinfo.latitude; 
+          res.data[0].longitude = res.data[0].fieldgeoinfo.longitude;  /**构建map需要的经纬度 */
+          // res.data[0].fieldgeoinfo = [{
+          //       id : 0,
+          //       iconPath: "../../images/icons/gps.png",
+          //       latitude:  res.data[0].fieldgeoinfo.latitude,
+          //       longitude:  res.data[0].fieldgeoinfo.longitude,
+          //       width:30,
+          //       height:30
+          // }]
+          /**构建地点marker坐标 */
+          _playerlists = res.data[0].playerlist;
+          this.userData._openid =  res.data[0].playerlist;
+          this.onGetIntoPlayer(res.data[0].playerlist)
+          /**将获取到的已报名的用户openid数组保存到pageData中 */
+          // const _ = db.command
+          //     /**嵌套查询用户表中符合_openid的player的用户信息 */
+          //         gamesPlayer.where({
+          //           _openid:_.in([_playerlists])
+          //         })
+          //         .get()
+          //         .then(
+          //           rest=>(  
+          //             console.log(rest),
+          //             this.setData({
+          //               playernames:rest.data
+          //             })
+          //             ))
+            this.setData({
+              playInfo:res.data
+            }),
+            res => {value();}
+              /**这里的逗号起到连接作用，和之前的语句形成一个语句串 */
+              /**构建一个箭头函数把callbcak作为返回值，实现了可以空值调用getData函数也有返回值的目的 */
+            wx.hideLoading();
+              /**判断当前用户的openid是否在已报名列中，如果没有则显示“报名这个活动”，如果存在则显示“退出这个活动” */
+
+            if(_playerlists.includes(uid)){
+              this.setData({
+                nickName:'insider',
+                uopenid:uid,
+                gameid:value
+              })
+            }else{
+              this.setData({
+                nickName:'nin'
+              })
+            }
     })
-   
+    return
   },
-  bindGetUserInfo(){
+  /**查询返回报名用户的信息 */
+  onGetIntoPlayer:function(value){
+
+    /**嵌套查询用户表中符合_openid的player的用户信息 */
+    db.collection('gamesPlayer').where({
+          _openid:_.in(value)
+        })
+        .get()
+        .then(
+          res=>(  
+            this.setData({
+              playernames:res.data
+            })
+            ))
+            return
+  },
+
+
+/**退出报名 */
+onCheckOut:function(e){
+  /**使用数组元素删除函数删除掉原所有报名者的数组，将数组重构 */
+   _tmparr = this.onCleanUser(this.userData._openid,e.currentTarget.dataset.userid);
+   /**错误点 */
+    // this.userData._openid.remove(e.currentTarget.dataset.userid);
+    // var _tmparr = [];
+    // for(var i=0; i <this.userData._openid.length ; i++){
+    //   _tmparr.push(this.userData._openid[i])
+    // };
+    
+    console.log(_tmparr)
+    // this.onDelPlayer(e.currentTarget.dataset.gameid,_tmparr)
+},
+
+/**数据清洗 */
+onCleanUser:function(str,id){
+    var _arr=[];
+    str.remove(id)
+    for(var i=0 ;i<str.length ; i++){
+      _arr.push(str._openid[i])
+        }
+      return _arr
+
+},
+
+onDelPlayer:function(id,arr){
+    /**将对象构建成一个数组_tmparr */
+    /**准备回传构建的报名者用户数组更新数据库中对应的id记录 */
+    db.collection('gamesSignUp').doc(
+      id
+    )
+    .update({
+        data:{
+          playerlist:arr
+        }
+    })
+    .then(res=>{
+      // this.onRefresh()
+    })
+    return
+},
+
+onCheckIn:function(e){
+
+},
+
+onRefresh:function(){
+    this.getData();
+  },
+  // onReachBottom:function(){
+  //   this.getData();
+  // }/**触底刷新 */
+bindGetUserInfo(){
     wx.getSetting({
       success: (result)=>{
         if (result.authSetting['scope.userInfo']){
@@ -161,36 +236,4 @@ Page({
       }}
     })
   },
-/**退出报名 */
-onCheckOut:function(e){
-  /**使用数组元素删除函数删除掉原所有报名者的数组，将数组重构 */
-  
-    this.pageData._alluserlist.remove(e.currentTarget.dataset.userid)
-    var _tmparr = []
-    for(var i=0; i <this.pageData._alluserlist.length ; i++){
-      _tmparr.push(this.pageData._alluserlist[i])
-    }/**将对象构建成一个数组_tmparr */
-    /**准备回传构建的报名者用户数组更新数据库中对应的id记录 */
-    gamesSignUp.doc(
-      e.currentTarget.dataset.gameid
-    )
-    .update({
-        data:{
-          playerlist:_tmparr
-        }
-    })
-    .then(console.log)
-},
-
-
-onCheckIn:function(e){
-
-},
-  // onRefresh:function(){
-  //   this.getData();
-  // },
-  // onReachBottom:function(){
-  //   this.getData();
-  // }/**触底刷新 */
-
 })
